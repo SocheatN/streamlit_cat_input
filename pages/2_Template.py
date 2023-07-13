4f7bdb24a78ca23c7b445b2b76c76e3a90fcd167
SocheatN <138544513+SocheatN@users.noreply.github.com>
2023-07-13 17:07:04 +0800

    Update 2_Template.py
    Delete unused functions

--- pages/2_Template.py
+++ pages/2_Template.py
@@ -13,54 +13,6 @@ st.set_page_config(page_title="Template", page_icon=":egg:", layout="wide")
 #st.markdown("# CAT Excel Template")
 
 # ---- DEF ----
-def generate_excel_download_link(df, file_name):
-    towrite = BytesIO()
-    df.to_excel(towrite,  index=False, header=True)  # write to BytesIO buffer
-    towrite.seek(0)  # reset pointer
-    b64 = base64.b64encode(towrite.read()).decode()
-    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download '+file_name+' </a>'
-    return st.markdown(href, unsafe_allow_html=True)
-
-def generate_html_download_link(fig):
-    towrite = StringIO()
-    fig.write_html(towrite, include_plotlyjs="cdn")
-    towrite = BytesIO(towrite.getvalue().encode())
-    b64 = base64.b64encode(towrite.read()).decode()
-    href = f'<a href="data:text/html;charset=utf-8;base64, {b64}" download="plot.html">Download Plot</a>'
-    return st.markdown(href, unsafe_allow_html=True)
-
-def loc_file_RMS(Exp,peril,df_Occ,df_Cons,df_BH, currency):
-    df=Exp.merge(df_Occ,on='LOBNAME').merge(df_Cons,on='LOBNAME').merge(df_BH,on='LOBNAME').merge(df_YB,on='LOBNAME') 
-    df['EQCV1VAL'],df['EQCV2VAL'],df['EQCV3VAL'],df['EQSITELIM'], df['WSCV1VAL'],df['WSCV2VAL'],df['WSCV3VAL'],df['WSSITELIM']=0,0,0,0,0,0,0,0
-    df[peril+'CV1VAL']=df['BLDG']*df['Occ_split']*df['Cons_split']*df['BH_split']*df['YB_split']
-    df[peril+'CV2VAL']=df['CONT']*df['Occ_split']*df['Cons_split']*df['BH_split']*df['YB_split']
-    df[peril+'CV3VAL']=df['BI']*df['Occ_split']*df['Cons_split']*df['BH_split']*df['YB_split']
-    df['NUMBLDGS']=np.ceil(df['NUMBLDGS']*df['Occ_split']*df['Cons_split']*df['BH_split']*df['YB_split'])
-    df[peril+'SITELIM']=df['SITELIM']*df['Occ_split']*df['Cons_split']*df['BH_split']*df['YB_split']
-    df=df[df[peril+'CV1VAL']+df[peril+'CV2VAL']+df[peril+'CV3VAL']>0]
-    
-    df['ACCNTNUM'],df['CNTRYCODE'],df['CNTRYSCHEME'],df['EQCV1VCUR'],df['EQCV2VCUR'],df['EQCV3VCUR'], df['EQSITELCUR'],df['WSCV1VCUR'],df['WSCV2VCUR'],df['WSCV3VCUR'],df['WSSITELCUR']= df['LOBNAME']+'_'+peril,'CN','ISO2A', currency, currency, currency, currency, currency, currency, currency, currency
-    
-    loc_file=df.drop(columns = ['BLDG','CONT','BI','TIV','SITELIM','Occupancy','Occ_split','Construction','Cons_split','BH','BH_split','YB','YB_split'])
-    return loc_file
-
-def loc_file_AIR(Exp,peril,df_Occ,df_Cons,df_BH, currency):
-    df=Exp.merge(df_Occ,on='ContractID').merge(df_Cons,on='ContractID').merge(df_BH,on='ContractID').merge(df_YB,on='ContractID') 
-    df['EQCV1VAL'],df['EQCV2VAL'],df['EQSITELIM'], df['WSCV1VAL'],df['WSCV2VAL'],df['WSSITELIM']=0,0,0,0,0,0
-    df[peril+'CV1VAL']=df['BuildingValue']*df['Occ_split']*df['Cons_split']*df['BH_split']*df['YB_split']
-    df[peril+'CV2VAL']=df['ContentsValue']*df['Occ_split']*df['Cons_split']*df['BH_split']*df['YB_split']
-    df=df[df[peril+'CV1VAL']+df[peril+'CV2VAL']>0]
-    
-    df['ContractID'], df['CNTRYCODE'], df['CNTRYSCHEME'], df['EQCV1VCUR'], df['EQCV2VCUR'], df['EQSITELCUR'], df['WSCV1VCUR'], df['WSCV2VCUR'], df['WSSITELCUR']= df['ContractID'], 'CN', 'ISO2A', currency, currency, currency, currency, currency, currency
-    df = df.loc[:,~df.columns.duplicated()].copy()
-    loc_file=df.drop(columns = ['BuildingValue','ContentsValue','Occupancy','Occ_split','Cons_split','BH_split','YB_split'])
-    return loc_file
-
-# Use local CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-        
 def generate_template_RMS(excel_template):
             buffer = BytesIO()
             xls = pd.ExcelFile(excel_template)
@@ -226,6 +178,12 @@ def generate_template_AIR_TC(excel_template):
                     label="Download Excel Template (AIR - Only TC)",
                     data=buffer,
                     file_name="AIR_TC_Template.xlsx")
+
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+        
 
 
 
