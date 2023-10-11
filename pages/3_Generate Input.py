f8555fe451b1a6facd7ac45fe561f6cf95328213
SocheatN <138544513+SocheatN@users.noreply.github.com>
2023-10-11 17:23:10 +0800

    Update 3_Generate Input.py
    Downlaod section

--- pages/3_Generate Input.py
+++ pages/3_Generate Input.py
@@ -14,8 +14,12 @@ st.set_page_config(page_title="Generate_Input", page_icon=":egg:", layout="wide"
 
 # ---- DEF ----
 def convert_into_csv(df, file_name):
-    downloads_path = str(Path.home()/"Downloads")
-    return df.to_csv(downloads_path+'//'+file_name+'.csv', index = False)
+    downloads_path = str(Path.home()/"Downloads")  
+    outdir = './downloads_path'  
+    if not os.path.exists(outdir):  
+        os.mkdir(outdir)        
+    fullname = os.path.join(outdir, file_name)      
+    return df.to_csv(fullname, index = False)
 
 
 def generate_download_button(button_label, df_Account, df_Location):
